0e61e8ae446d3bb4ec017ae426101fd2d82f68cd
RealJt <1562277363@qq.com>
2017-06-17 00:46:38 +0800

    tomcat启动完成

---
+++ summer/src/main/java/com/realjt/summer/summer/SummerServlet.java
@@ -0,0 +1,37 @@
+package com.realjt.summer.summer;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SummerServlet extends HttpServlet
+{
+	private static final long serialVersionUID = -3903640785697216419L;
+
+	private static final Logger log = LoggerFactory
+			.getLogger(SummerServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		response.setCharacterEncoding("UTF-8");
+
+		log.info("abc");
+
+		response.getWriter().write("abc");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+	}
+
+}
